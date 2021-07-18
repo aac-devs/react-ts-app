1c4e17c9d91039f66ce11d59c2a9ac45fa066518
Andrés <aac.devs@gmail.com>
2021-07-17 23:02:57 -0300

    project before context API

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+
+import classes from "./NewTodo.module.css";
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInput = useRef<HTMLInputElement>(null);
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInput.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" name="" id="text" ref={todoTextInput} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
